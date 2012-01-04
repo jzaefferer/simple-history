525e5862290c49703ce57a49cf98edc9a43186b2
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-01-04 13:12:45 +0100

    Replace timeout with a more explicit check to avoid initial popstate event in Chrome.

--- simple-history.js
+++ simple-history.js
@@ -1,10 +1,12 @@
 /*!
- * Simple History v0.3.0
+ * Simple History v0.4.0
  *
  * Copyright 2011, Jörn Zaefferer
  * Dual licensed under the MIT or GPL Version 2 licenses.
  */
 (function(window, undefined) {
+
+  var initial = location.href;
 
   window.SimpleHistory = {
     supported: !!(window.history && window.history.pushState),
@@ -20,12 +22,15 @@
     },
     start: function(matcher) {
       this.matcher = matcher;
-      // delay binding to ignore first popstate event in Chrome
-      setTimeout(function() {
-        window.addEventListener("popstate", function() {
-          SimpleHistory.notify();
-        }, false);
-      }, 100);
+      window.addEventListener("popstate", function() {
+        // workaround to always ignore first popstate event (Chrome)
+        // a timeout isn't reliable enough
+        if (initial && initial === location.href) {
+          initial = null;
+          return;
+        }
+        SimpleHistory.notify();
+      }, false);
     }
   };
 
